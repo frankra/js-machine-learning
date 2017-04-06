7e24932bf99690084498b18c676d43a1463785b5
Rafael Frank <rafael.frank@sap.com>
2017-04-06 10:30:50 -0300

    Adding LinearRegressionWithOneVar

---
+++ LinearRegressionOneFeature/charts.js
@@ -0,0 +1,54 @@
+(function () {
+  "use strict";
+
+  function Charts() { }
+
+  Charts.prototype.plotLine = function (svg, aData, sXAttribute, sYAttribute) {
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    var width = +svg.attr("width") - margin.left - margin.right;
+    var height = +svg.attr("height") - margin.top - margin.bottom;
+    var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleLinear()
+      .rangeRound([0, width]);
+
+    var y = d3.scaleLinear()
+      .rangeRound([height, 0]);
+
+    var line = d3.line()
+      .x(function (d) { return x(d[sXAttribute]); })
+      .y(function (d) { return y(d[sYAttribute]); });
+
+    x.domain(d3.extent(aData, function (d) { return d[sXAttribute]; }));
+    y.domain(d3.extent(aData, function (d) { return d[sYAttribute]; }));
+
+    g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .select(".domain")
+      .remove();
+
+    g.append("g")
+      .call(d3.axisLeft(y))
+      .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text(sYAttribute);
+
+    g.append("path")
+      .datum(aData)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+      
+  }
+
+  window.top.Charts = new Charts();
+
+})();
---
+++ LinearRegressionOneFeature/implementation.js
@@ -0,0 +1,82 @@
+(function () {
+  "use strict";
+  
+  function LinearRegressionOneFeature(){}
+
+  LinearRegressionOneFeature.prototype.aTrainingSet = [
+    { x: 1, y: 1 },
+    { x: 2, y: 2 },
+    { x: 3, y: 3 }
+  ]
+  //Used to store the Squared Error Results
+  LinearRegressionOneFeature.prototype.aSquaredErrorResults = [];
+
+  /*
+  4       x
+  3     x
+  2   x
+  1 x 
+  0 1 2 3 4
+  */
+
+  //hϴ(x) = 0 + ϴ1 * x 
+  LinearRegressionOneFeature.prototype.hipotesis = function(x, vTetha1) {
+    return vTetha1 * x
+  }
+
+  LinearRegressionOneFeature.prototype.squaredError = function(vPossibleTetha1) {
+    var vLinearRegressionResult = 0;
+    for (var i = 0, ii = this.aTrainingSet.length; i < ii; i++) {
+      vLinearRegressionResult += Math.pow(this.hipotesis(this.aTrainingSet[i].x, vPossibleTetha1) - this.aTrainingSet[i].y, 2);
+    }
+    return (1 / (2 * this.aTrainingSet.length)) * vLinearRegressionResult;
+  }
+
+  LinearRegressionOneFeature.prototype.findLowestSquaredErrorResult = function(vCurrentSquaredErrorResult, vTethaTest, vVarianceFactor) {
+    var vIncreasedTetha = vTethaTest + vVarianceFactor;
+    var vDecreasedTetha = vTethaTest - vVarianceFactor;
+    var vIncreasingTethaSQEResult = this.squaredError(vIncreasedTetha);
+    var vDecreasingTethaSQEResult = this.squaredError(vDecreasedTetha);
+    //Return on Zero
+    if (vIncreasingTethaSQEResult === 0) {
+      this.aSquaredErrorResults.push({
+        tetha: vIncreasedTetha,
+        sqrdErr: vIncreasingTethaSQEResult
+      });
+      return vIncreasedTetha;
+    } else if (vDecreasingTethaSQEResult === 0) {
+      this.aSquaredErrorResults.push({
+        tetha: vDecreasedTetha,
+        sqrdErr: vDecreasingTethaSQEResult
+      });
+      return vDecreasedTetha;
+    }
+
+    if (vIncreasingTethaSQEResult < vCurrentSquaredErrorResult) {
+      this.aSquaredErrorResults.push({
+        tetha: vIncreasedTetha,
+        sqrdErr: vIncreasingTethaSQEResult
+      });
+      return this.findLowestSquaredErrorResult(vIncreasingTethaSQEResult, vIncreasedTetha, vVarianceFactor); //Route to Increase
+    } else if (vDecreasingTethaSQEResult < vCurrentSquaredErrorResult) {
+      this.aSquaredErrorResults.push({
+        tetha: vDecreasedTetha,
+        sqrdErr: vDecreasingTethaSQEResult
+      });
+      return this.findLowestSquaredErrorResult(vDecreasingTethaSQEResult, vDecreasedTetha, vVarianceFactor); //Route to Decrease
+    } else if (vVarianceFactor === 0) {
+      this.aSquaredErrorResults.push({
+        tetha: vIncreasedTetha,
+        sqrdErr: vDecreasingTethaSQEResult
+      });
+      return vIncreasedTetha;
+    } else {
+      return this.findLowestSquaredErrorResult(vIncreasingTethaSQEResult, vTethaTest, vVarianceFactor / 2); //Reduce Variance and try again
+    }
+  }
+
+  window.top.LinearRegressionOneFeature = new LinearRegressionOneFeature();
+
+})();
+
+
